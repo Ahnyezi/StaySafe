50de0d99b8797a5a8757838c82eac7bcd9bfebce
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2022-02-10 23:43:54 +0900

    [CHORE] front 디렉토리 구조 설정 및 메인페이지 기본 설정

---
+++ StaySafe_vue-client/src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input
+      v-model="searchWord"
+      :placeholder="placeholder"
+      @keypress="handleSearchWord"
+    />
+    <button @click="setSearchWord">검색</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchWord: "",
+      placeholder: "복용 중인 약 이름을 입력하세요",
+    };
+  },
+  methods: {
+    handleSearchWord({ keyCode }) {
+      const { text } = this;
+      console.log("text" + text);
+      if (keyCode === 13 && text !== "") {
+        this.$emit("getSearchWord", text);
+        console.log(text);
+      }
+    },
+    setSearchWord() {
+      this.$emit("getSearchWord", this.searchWord);
+      this.searchWord = "";
+    },
+  },
+  watch: {
+    searchWord(text) {
+      this.$emit("getRealtimeSearchWord", text);
+    },
+  },
+};
+</script>
+
+<style></style>
